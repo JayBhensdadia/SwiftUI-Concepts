cecf0f67825d3ff9e70710c56b80eccde0a078c6
Jay Bhensdadia <jayvpatel29@gmail.com>
2023-03-03 07:24:33 +0530

    Initial Commit

---
+++ SwiftUI Concepts/SwiftUI_ConceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ConceptsApp.swift
+//  SwiftUI Concepts
+//
+//  Created by Jay Bhensdadia on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ConceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
